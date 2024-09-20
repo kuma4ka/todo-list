b09296ff402807d95c571e91ed57eec98dcd1986
Dmytro <81484485+kuma4ka@users.noreply.github.com>
2024-09-20 15:53:02 +0200

    Initial commit

---
+++ ToDoList/ToDoList.Api/Program.cs
@@ -0,0 +1,9 @@
+namespace ToDoList.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
---
+++ ToDoList/ToDoList.Api/Startup.cs
@@ -0,0 +1,14 @@
+namespace ToDoList.Api;
+
+public class Startup(IConfiguration configuration)
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        
+    }
+}
